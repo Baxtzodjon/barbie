802faa454cd7fd96c407b0321ac5b55facd7b9c9
Baxtzodjon <rasulovbaxtzod@gmail.com>
2023-09-08 18:50:24 +0500

    safir durak

---
+++ script.js
@@ -0,0 +1,26 @@
+/* let num = 20.49
+let numTwo = 22 */
+
+/* let arr = ['abdukhafiz', 'alsafir', 'bobur', 'islom', 'aziza', 'azizabonu', 'xojik', 'baxtzod', 'abdumanon', 'safir', 'shakhzod', 'akbarshox']
+arr[Math.floor(Math.random() * 13)] + " durak" */
+
+let num = 20.699213123
+
+console.log(
+    /* Math.ceil(num) */ /* округяет цифру в потолок */
+    /* Math.floor(num) */ /* округяет цифру в пол */
+    /* Math.round(num) */ /* округляет до ближайшего целово */
+
+    /* Math.max(num, numTwo) */
+    /* Math.min(num, numTwo) */
+
+    /*  parseInt(num) */ /* переводит из стороки в число убирая запятые */
+    /* parseFloat(num) */ /* переводит из стороки в число */
+   
+    /* num.toFixed(1) */
+    
+
+    /* git config --global user.name "baxtzod"
+    git config --global user.email "baxtzod@gmail.com" */
+
+);
